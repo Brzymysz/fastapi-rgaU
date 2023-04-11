c831b533e4f2d04f1221a304422c811237a58901
AvadEu <dutwik44@wp.pl>
2023-04-11 11:39:07 +0200

    Update CORS Policy

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from random import sample
 import uvicorn
 import sqlite3
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 def query_db(query_str: str) -> tuple:
     try:
